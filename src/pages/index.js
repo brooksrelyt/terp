8ffe0f3cd93865ba17d32b02ae4ae8c47e515718
Tyler Brooks <brooksrelyt@gmail.com>
2019-02-21 15:56:46 -0500

    homepage changes made

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,16 +68,61 @@ const IndexPage = () => (
                 </CardBody>
               </Card>
             </div>
+
+            <div className="col-md-6">
+              <Card className="mb-5">
+                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                </CardBody>
+              </Card>
+            </div>
+
+            <div className="col-md-6">
+              <Card className="mb-5">
+                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                </CardBody>
+              </Card>
+            </div>
+            
           </div>
         </section>
         <aside className="col-3">
-          <ListGroup flush>
-            <ListGroupItem disabled tag="a" href="#">Cras justo odio</ListGroupItem>
-            <ListGroupItem tag="a" href="#">Dapibus ac facilisis in</ListGroupItem>
-            <ListGroupItem tag="a" href="#">Morbi leo risus</ListGroupItem>
-            <ListGroupItem tag="a" href="#">Porta ac consectetur ac</ListGroupItem>
-            <ListGroupItem tag="a" href="#">Vestibulum at eros</ListGroupItem>
-          </ListGroup>
+          <div>
+            <ListGroup flush className="mb-3">
+              <ListGroupItem disabled tag="a" href="#">Recent Posts</ListGroupItem>
+              <ListGroupItem tag="a" href="#">Dapibus ac facilisis in</ListGroupItem>
+              <ListGroupItem tag="a" href="#">Morbi leo risus</ListGroupItem>
+              <ListGroupItem tag="a" href="#">Porta ac consectetur ac</ListGroupItem>
+              <ListGroupItem tag="a" href="#">Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+          </div>
+
+          <div>
+            <ListGroup flush className="mb-3">
+              <ListGroupItem disabled tag="a" href="#">Web Only</ListGroupItem>
+              <ListGroupItem tag="a" href="#">Dapibus ac facilisis in</ListGroupItem>
+              <ListGroupItem tag="a" href="#">Morbi leo risus</ListGroupItem>
+              <ListGroupItem tag="a" href="#">Porta ac consectetur ac</ListGroupItem>
+              <ListGroupItem tag="a" href="#">Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+          </div>
+
+          <div>
+            <ListGroup flush className="mb-3">
+              <ListGroupItem disabled tag="a" href="#">Popular Posts</ListGroupItem>
+              <ListGroupItem tag="a" href="#">Dapibus ac facilisis in</ListGroupItem>
+              <ListGroupItem tag="a" href="#">Morbi leo risus</ListGroupItem>
+              <ListGroupItem tag="a" href="#">Porta ac consectetur ac</ListGroupItem>
+              <ListGroupItem tag="a" href="#">Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+          </div>
         </aside>
       </div>
     </div>
